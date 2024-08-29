0ae580b9439d5e17dcb51b1c44ef752f94c6e4c4
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-29 04:36:50 -0700

    extra component just for main page

---
+++ src/pages/main/Index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Header } from '../../components/header/Header'
+import AppRoutes from '../../routes/Index'
+
+export const Index = () => {
+  return (
+    <div>
+        <div>
+        <Header />
+        </div>
+    
+        <div>
+        <AppRoutes />
+        </div>
+    
+    </div>
+
+  )
+}

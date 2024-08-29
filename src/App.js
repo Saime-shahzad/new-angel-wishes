ea48ec60dbe72755756e008296cf2a40d69467a1
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-29 04:34:19 -0700

    routing updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Index } from './pages/main/Index';
 
 function App() {
   return (
-    <div className="bg-info">
-       new angel wishes
+    <div className="">
+      {/* <AppRoutes /> */}
+      {/* <Home /> */}
+      <Index />
        
     </div>
   );

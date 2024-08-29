93bdab2e55eb0dd3324dd8ed745eec4389d45558
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-29 04:35:00 -0700

    new icons addedd

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,17 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import webColor from '../colors/Colors';
+
+const icons=
+    {facebookIcons: < FacebookIcon style={{ color: webColor.themeColor }}/>,
+    youtubeIcons: < YouTubeIcon style={{ color: webColor.themeColor, fontSize:"28px" }}/>,
+    instaIcons: < InstagramIcon style={{ color: webColor.themeColor }}/>,
+    pintrestIcons: < PinterestIcon style={{ color: webColor.themeColor }}/>,
+
+
+}
+
+
+export default icons

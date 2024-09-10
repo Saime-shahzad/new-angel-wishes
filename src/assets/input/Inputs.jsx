633fb835748828fb1cbc2b4ebb338ed94af5aeb8
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 03:52:19 -0700

    dynamic input compionent

---
+++ src/assets/input/Inputs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import "./Inputs.css"
+import { InputAdornment } from '@mui/material';
+
+export const Inputs = ({type , labelName , icon ,placeholder, className, suffix}) => {
+  return (
+    <div>
+        <div className='textFieldparrent mb-2'>
+            <div>
+                {labelName}
+            </div>
+        <TextField className={ className ? className :'textFieldClass '}
+       
+            endAdornment={ <InputAdornment position="start">kg</InputAdornment>}
+            
+    label={icon ? icon :labelName}
+
+    placeholder={placeholder}
+    multiline={labelName === "Message" ? true : false}
+    
+    type={type}
+    variant="filled"
+  />
+  </div>
+  
+  </div>
+  )
+}

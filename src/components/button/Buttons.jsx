43142a813aa93af6e13fd29a2215e77f4112f48b
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 03:55:12 -0700

    class added

--- src/components/button/Buttons.jsx
+++ src/components/button/Buttons.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Buttons = ({text , type , className, onClick , style}) => {
   return (
-    <div className='button-parent'>
+    <div className='button-parent '>
 
 <Button variant="contained" className={className} style={style} type={type} onClick={onClick}>{text}</Button>
     </div>

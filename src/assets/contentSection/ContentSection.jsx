094539cda92950f67f3d665999d55255c877b4a9
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 03:50:28 -0700

    component added for dynamic routing

---
+++ src/assets/contentSection/ContentSection.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// import "./ContentStyle.css"
+
+export const ContentSection = ({
+
+  data,
+}) => {
+  return (
+    <div className="ContentSection-parent my-5 ">
+      <div className="chaild-1 container">
+        {data?.map((item) => {
+          return (
+            <div
+            //   className="d-flex "
+              className={item.className}
+            >
+              {/* <div className="text-section  text-center  "> */}
+              <div className= "text-section  col-lg-6 col-md-6 col-sm-12 " >
+                
+                <div className=" ">
+                <h1>{item.heading}</h1>
+                <h5>{item.subHeading}</h5>
+                <div className="">{item.text}</div>
+                </div>
+              </div>
+              
+              
+              {/* <div className="  img-section "> */}
+              <div className= {item.possition === "left" ?  "img-section col-lg-6 col-md-6 col-sm-12" :"col-lg-6 col-md-6 col-sm-12 d-flex justify-content-end"}>
+                {item.img ? 
+                <img src={item.img} width="400px" height="400px" alt="home-1" /> : ""}
+              </div>
+              {/* ///extra content section */}
+
+                        {item.extraContent && <div>
+                            {item.extraContent}
+                            
+                            </div>}
+              {/* ///closed/// */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+
+
+  );
+};

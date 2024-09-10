98ce3ea8e931fdfbf9cec27bf107945dc31c871f
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 04:31:42 -0700

    new routes added

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Routes, Route  } from "react-router-dom";
 import { AboutUs } from "../pages/about/AboutUs";
 import { Home } from "../pages/home/Home";
+import { Career } from "../pages/carrer/Career";
+import { Contact } from "../pages/contact/Contact";
+import { PrivacyPolicy } from "../pages/privacyPolicy/PrivacyPolicy";
+import { TermsCondition } from "../pages/termsAndCondition/TermsCondition";
+import { Legacies } from "../pages/legacies/Legacies";
+import { KidsCorner } from "../pages/kidsCorner/KidsCorner";
+import { MissionVission } from "../pages/mission&Vission/MissionVission";
+import { ReportaConcern } from "../pages/reportAconcern/ReportaConcern";
+import { AccessibilityReports } from "../pages/accessibility-reports/AccessibilityReports";
+import { Faqs } from "../pages/faqs/Faqs";
 // import { publicRoutesList } from "./Routes";
 
 const AppRoutes = () => {
@@ -9,6 +19,16 @@ const AppRoutes = () => {
         // { path: "/", element: <Home /> },
         { path: "/",exect:"exect", element: <Home/> },
         { path: "/about-us", element: <AboutUs /> },
+        { path: "/carrer", element: <Career /> },
+        { path: "/contact-us", element: <Contact /> },
+        { path: "/privacy-policy", element: <PrivacyPolicy /> },
+        { path: "/terms&condtions", element: <TermsCondition /> },
+        { path: "/legacies", element: <Legacies /> },
+        { path: "/kidsCorner", element: <KidsCorner /> },
+        { path: "/mission&vission", element: <MissionVission /> },
+        { path: "/report-a-concern", element: <ReportaConcern /> },
+        { path: "/accessibility-reports", element: <AccessibilityReports /> },
+        { path: "/faqs", element: <Faqs /> },
        
       ];
   return (

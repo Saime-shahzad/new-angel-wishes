13b8193a45ac6822cc6456e8c8932fbe6f478713
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 03:51:06 -0700

    new icons added

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -3,12 +3,23 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 import webColor from '../colors/Colors';
+import Person3Icon from '@mui/icons-material/Person3';
+import EmailIcon from '@mui/icons-material/Email';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MessageIcon from '@mui/icons-material/Message';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 const icons=
     {facebookIcons: < FacebookIcon style={{ color: webColor.themeColor }}/>,
     youtubeIcons: < YouTubeIcon style={{ color: webColor.themeColor, fontSize:"28px" }}/>,
     instaIcons: < InstagramIcon style={{ color: webColor.themeColor }}/>,
     pintrestIcons: < PinterestIcon style={{ color: webColor.themeColor }}/>,
+    manIcons: < Person3Icon style={{ color: webColor.themeColor }}/>,
+    mailIcons: < EmailIcon style={{ color: webColor.themeColor }}/>,
+    phoneicons: < LocalPhoneIcon style={{ color: webColor.themeColor }}/>,
+    messageicons: < MessageIcon style={{ color: webColor.themeColor }}/>,
+    expandMoreIcons: < ExpandMoreIcon style={{ color: "white" }}/>,
 
 
 }

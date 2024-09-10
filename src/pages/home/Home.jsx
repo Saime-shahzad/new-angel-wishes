a7465af6b41b9dbc2e22cac3c24d8e290980b522
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 04:26:27 -0700

    content updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,14 +6,67 @@ import ban3 from "../../assets/images/ban3.png"
 import { Buttons } from "../../components/button/Buttons";
 import webColor from "../../assets/colors/Colors";
 import './Home.css'
+import home1 from "../../assets/images/home1.png"
+import home2 from "../../assets/images/home2.png"
+import home3 from "../../assets/images/home3.png"
+import home4 from "../../assets/images/home4.png"
+import { ContentSection } from "../../assets/contentSection/ContentSection";
 
 export const Home = () => {
-  return (
-    <div>
 
-      {/* Header section  */}
+  const homePageContent=[
+
+    {
+      // heading:"Save Your Memories",
+      heading:<span>Save Your <span style={{color:webColor.themeColor}}>Memories</span></span>,
+      subHeading:"We Allow Families to Cherish  Every Moment",
+      possition:"left",
+      text:"Every moment in life counts. At Angel Wishes Memory Keeper, we provide a secure and loving space where you and your loved ones can preserve these precious memories. Whether it's photos, videos, or written stories, our platform ensures that your life's highlights are safely stored and easily accessible. Start today and keep your cherished memories alive for generations to come. Experience the comfort of knowing that your most significant moments are preserved with care and respect.",
+      className:"content-Parent  row my-4 flex-lg-row-reverse  ",
+      img:home1
+    },
+
+    {
+      heading:<span style={{color:webColor.themeColor}}> Secure <span>Your</span></span>,
+      subHeading:"We Provide a Safe and Secure Place for Your Most Important Documents",
+      possition:"right",
+      text:"Your privacy and the security of your data are our top priorities. At Angel Wishes Memory Keeper, we protect your information with advanced encryption and robust security protocols. Our platform operates on a custom AWS secure configuration, providing 400 terabytes of dedicated, safe storage. With 99.98% uptime, you can trust that your sensitive documents, from wills to power of attorney forms, are protected around the clock. Secure your legacy with confidence, knowing your data is safe.",
+      className:"content-Parent  row my-4    ",
+      img:home2
+    },
+    {
+      heading:"Terms And Conditions",
+      subHeading:"",
+      possition:"right",
+      text:"These Terms shall be governed and construed in accordance with the laws of the state where Angel Wishes Memory Keeper is headquartered, without regard to its conflict of law provisions.",
+      className:" w-100 middleSection  row ",
+      img:false
+    },
+
+    {
+      heading:<span>Get  <span style={{color:webColor.themeColor}}>Consulting</span> Assistance</span>,
+      subHeading:"Expert Guidance When You Need It Most",
+      possition:"left",
+      text:"Navigating final wishes planning can be complex and emotional. That's why Angel Wishes Memory Keeper offers compassionate and professional final wishes consulting. Our experts are here to guide you through every step of the process, from estate planning to creating a living trust. We ensure you have all the resources and support needed to make informed decisions that honor your wishes and provide peace of mind for your family. Reach out today to learn how our consulting services can help simplify this critical journey",
+      className:"content-Parent  row my-5 flex-lg-row-reverse justify-content-between   ",
+      img:home3
+    },
+    
+    {
+      heading:<span>Begin Your Journey with Memory  Keeper <span style={{color:webColor.themeColor}}>Angel Wishes</span> Assistance</span>,
+      // subHeading:"subheading",
+      possition:"right",
+      text:"Are you ready to secure your memories, safeguard your data, and prepare your final wishes with expert guidance? Click below to explore our services and discover how Angel Wishes Memory Keeper can support you and your family through every step of the journey",
+      className:"content-Parent  row my-5   ",
+      img:home4
+    },
+
+    
+  ]
+  return (
+    <div className="homeParrent">
+
       
-      {/* ////closed */}
 
     {/* banner Section */}
     <div className="banerSection-parent">
@@ -43,9 +96,9 @@ export const Home = () => {
   
 </div>
 
-<div class="position-absolute text-white text-sm-start d-none d-md-block" style={{top:"160px" , left:"30px"}}>
+<div class="position-absolute BannerTextControl text-white text-sm-start  d-md-block" >
 
-      <div className="d-flex">
+      <div className="d-flex  custom-animate-left">
         <div className="logo-parent">
     <img class=" px-3"  height="200px" src={angell} alt="First slide"/>
     </div>
@@ -54,10 +107,10 @@ export const Home = () => {
         Welcom To
       </span>
     <h5 className="fw-normal" style={{fontSize:"45px"}}>ANGEL WISHES</h5>
-    <p className="w-50" style={{fontSize:"10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic atque sint quo, quam dignissimos provident magnam corrupti perspiciatis voluptates 
+    <p className="w-50 d-none d-md-block" style={{fontSize:"10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic atque sint quo, quam dignissimos provident magnam corrupti perspiciatis voluptates 
       enim sapiente quisquam ea debitis rerum earum, beatae quasi excepturi. Maxime!</p>
       <div className="d-flex ">
-      <Buttons  text='Get A Quote ' className='bg-white mx-1' style={{color:webColor.themeColor}} />
+      <Buttons  text='Get A Quote ' className='bg-white  mx-1' style={{color:webColor.themeColor}} />
       <Buttons  text='Get A Quote '  style={{color:"white" , backgroundColor:webColor.themeColor}} />
       </div>
       </div>
@@ -68,7 +121,14 @@ export const Home = () => {
 
     {/* ///closed */}
 
-      {/* <AppRoutes /> */}
+      {/* /////// middle section/////// */}
+
+      <div>
+        <ContentSection  data={homePageContent} />
+      </div>
+
+
+      {/* /////closed////// */}
     </div>
   );
 };

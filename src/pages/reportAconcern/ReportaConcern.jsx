3040e72627b3026a8536344be2fe0d6b7f81e62a
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-10 04:30:40 -0700

    new file new content

---
+++ src/pages/reportAconcern/ReportaConcern.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { CardSections, CommonParagraph } from "../../assets/others/Others";
+import report1 from "../../assets/images/report1.png";
+import report2 from "../../assets/images/report2.png";
+import report3 from "../../assets/images/report3.png";
+import { Inputs } from "../../assets/input/Inputs";
+import "./ReportaConcern.css";
+import { Buttons } from "../../components/button/Buttons";
+import icons from "../../assets/icons/icons";
+
+export const ReportaConcern = () => {
+  const cardsData = [
+    {
+      heading: "Client Support Specialist",
+      paragraphText:
+        "Provide direct assistance and support to our clients, helping them navigate our services and ensuring they receive the care and information they need.",
+      image: report1,
+      className:
+        "col-lg-4 col-md-6 col-sm-12 box-shadow text-center p-0 d-flex justify-content-around",
+      imageSize: {
+        width: "120px",
+        height: "120px",
+      },
+    },
+    {
+      heading: "Legal Consultant",
+      paragraphText:
+        "Specialize in estate planning, wills, and trusts to provide expert advice and peace of mind to our clients and their families.",
+      image: report2,
+      className:
+        "col-lg-4 col-md-6 col-sm-12 box-shadow text-center p-0 d-flex justify-content-around",
+      imageSize: {
+        width: "120px",
+        height: "120px",
+      },
+    },
+    {
+      heading: "Legal Consultant",
+      paragraphText:
+        "Specialize in estate planning, wills, and trusts to provide expert advice and peace of mind to our clients and their families.",
+      image: report3,
+      className:
+        "col-lg-4 col-md-6 col-sm-12 box-shadow  text-center p-0 d-flex justify-content-around",
+      imageSize: {
+        width: "120px",
+        height: "120px",
+      },
+    },
+  ];
+  const formContent = [
+    {
+      label: "User Name",
+      type: "text",
+      className:"col-lg-6 col-md-6 col-sm-12   d-flex inputDiv"
+    },
+    {
+        label: "Email Address",
+        type: "email",
+        className:"col-lg-6 col-md-6 col-sm-12   d-flex inputDiv"
+    },
+    {
+      label: "Contact No",
+      type: "Number",
+      placeholder: "",
+      className:"col-lg-6 col-md-6 col-sm-12   d-flex inputDiv"
+    },
+    {
+      label: "Supporting Document",
+      type: "file",
+      placeholder: "",
+      className:"col-lg-6 col-md-6 col-sm-12   d-flex inputDiv"
+    },
+    {
+      label: "Message",
+      type: "text",
+      placeholder: "",
+      className:"col-lg-12 col-md-12 col-sm-12 w-100  messageControls d-flex  inputDiv"
+    },
+    {
+        label: "Submit",
+        type: "Submit",
+        className:"col-lg-6 col-md-6 col-sm-12 justify-content-end m-auto d-flex inputDiv"
+    },
+  ];
+  return (
+    <div className="parent">
+      <div className="child-1 my-5 ">
+        <CommonParagraph
+          headingText="How to Report a Concern"
+          paragraphText="To ensure that your concern is addressed efficiently, please follow these steps:"
+          className="text-center"
+          style={{ fontSize: "16px" }}
+        />
+      </div>
+      <div className="cards-setion child-2 container my-5">
+        <CardSections data={cardsData} />
+      </div>
+      <div
+        className="child-3 my-5 p-3 form-Section container"
+        style={{ backgroundColor: "#1C96C5" }}
+      >
+        <form
+          className="my-5 container control-width rounded-2  "
+          style={{ backgroundColor: "#E9EFEF", border: "8px solid white" }}
+        >
+          <div className="heading-contant p-2 ">
+            <h1>Report Form</h1>
+            <p>
+              Please complete the form below to submit your concern. All
+              information provided will be handled confidentially.
+            </p>
+          </div>
+          <div className="from-body  d-flex row  ">
+            {formContent?.map((item) => {
+              return (
+                <div
+                  className={item.className}
+                 
+                >
+                    {item.type === "Submit" ? (
+                        <Buttons text={item.label} style={{backgroundColor:"#B89313"}} type={item.type}/>
+                    ) : (
+                    <Inputs
+                    className={item.type === "file" ? "file-input-container" : "inputsControls"}
+                    //   className= {item.label === "Message" ? " d-none " : " inputsControls  "}
+                      type={item.type}
+                      icon={ item.label}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <div className="d-flex  flex-column flex-lg-row my-2">
+            <div className="">
+                <div>
+                {icons.phoneicons}
+                </div>
+
+                <div>
+                (555) 123-4567
+                </div>
+            </div>
+            <div className=" mx-lg-4 ">
+                <div>
+                {icons.mailIcons}
+                </div>
+
+                <p className="w-100">
+                concerns@angelwishesmemorykeeper.com
+                </p>
+            </div>
+         
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/termsAndCondition/TermsCondition.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { CommonParagraph } from "../../assets/others/Others";
+
+export const TermsCondition = () => {
+  const termsConditionContent = [
+    {
+      headingText: "Terms and Conditions",
+      paragraphText:
+        "Welcome to Angel Wishes Memory Keeper! You agree to comply with and be bound by the following terms and conditions by accessing and using our website, services, and tools. Please review these terms carefully. If you disagree with these terms, you should not use this site or any of its services or contact us with any questions.",
+    },
+    {
+      headingText: "Use of the Website",
+      paragraphText: (
+        <div>
+          <h5>License to Use Website</h5>
+          <p>
+            Angel Wishes Memory Keeper grants you a non-exclusive,
+            non-transferable, revocable license to use the website strictly in
+            accordance with these terms.
+          </p>
+          <h5>User Conduct</h5>
+          <p>
+            You agree not to use the site in a way that may impair its
+            performance, corrupt its content, or otherwise reduce its overall
+            functionality. You also agree not to compromise the
+          </p>
+          <h5>Business Transfers</h5>
+          <p>
+            Your personal information may be transferred in the event of a
+            merger, acquisition, or asset sale. We will provide notice before
+            your personal information is transferred and becomes subject to a
+            different privacy policy.
+          </p>
+        </div>
+      ),
+    },
+    {
+      headingText: "Intellectual Proper",
+      paragraphText:
+        "The content, layout, design, data, databases, and graphics on this website are protected by intellectual property laws. The content is solely owned by Angel Wishes Memory Keeper unless otherwise indicated. Except as expressly stated herein, you may not reproduce, copy, distribute, or create derivative works from this content without the express written permission of Angel Wishes Memory Keeper.",
+    },
+    {
+      headingText: "User Accounts",
+      paragraphText:
+        "If you create an account at Angel Wishes Memory Keeper, you are responsible for maintaining its security and for all activities that occur under it. You must immediately notify us of any unauthorized use of your account or other security breaches.",
+    },
+    {
+      headingText: "Links to Other Websites",
+      paragraphText:
+        "Our Service may contain links to third-party websites or services that are not owned or controlled by Angel Wishes Memory Keeper. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party websites or services.",
+    },
+    {
+      headingText: "Limitation of Liability",
+      paragraphText:
+        "Angel Wishes Memory Keeper will not be liable for any consequential, incidental, indirect, exemplary, punitive, or special damages of any kind, however, caused, including loss of profits, revenue, data, or use, incurred by you, whether under theory of contract, tort (including negligence), warranty or otherwise, irrespective of whether we have been advised of the possibility of such damages.",
+    },
+    {
+      headingText: "Indemnification",
+      paragraphText:
+        "You agree to indemnify, defend, and hold harmless Angel Wishes Memory Keeper, its officers, directors, employees, agents, licensors, suppliers, and any third-party information providers to the service from and against all losses, expenses, damages, and costs, including reasonable attorneys' fees, resulting from any violation of these Terms and Conditions by you.",
+    },
+    {
+      headingText: "Governing Law",
+      paragraphText:
+        "These Terms shall be governed and construed in accordance with the laws of the state where Angel Wishes Memory Keeper is headquartered, without regard to its conflict of law provisions",
+    },
+    {
+      headingText: "Contact Us",
+      paragraphText: (
+        <div>
+          <p>
+            if you have any questions about these Terms, please contact us at:
+          </p>
+          <ul>
+            <li>Email: contact@angelwishesmemorykeeper.com</li>
+            <li>Phone: (555) 123-4567</li>
+          </ul>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="parrent">
+      <div className="child-1 container mt-5">
+        {termsConditionContent?.map((item) => (
+          <CommonParagraph
+            className=""
+            style={{ fontSize: "16px" }}
+            paragraphText={item.paragraphText}
+            headingText={item.headingText}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};

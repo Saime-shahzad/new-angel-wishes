6c6ec41ca0e0d9cf07e6a09ac76aac73a71896d8
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-09-11 05:15:46 -0700

    popup for  header menue added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 // import icons from "../../assets/icons/icons";
 import haderLogo from "../../assets/images/logo.png";
@@ -9,9 +9,23 @@ import profilewhiteImage from "../../assets/images/manlogowhite.png";
 import menueWhite from "../../assets/images/menuewhite.png";
 import { Link, useLocation } from "react-router-dom";
 import webColor from "../../assets/colors/Colors";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import useRoutFunction from "../../assets/others/UseFullFunctions";
 
 export const Header = () => {
   const location = useLocation();
+  const routeTo = useRoutFunction();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isPopupName, setIsPopupName] = useState("");
+  const open = Boolean(anchorEl);
+  const handleClick = (event, name) => {
+    setIsPopupName(name);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const headerItems = [
     {
@@ -26,8 +40,8 @@ export const Header = () => {
     },
     {
       tabName: "Career",
-      link: "/carrer",
-      style: location.pathname === "/carrer" ? "underline" : "none",
+      link: "/career",
+      style: location.pathname === "/career" ? "underline" : "none",
     },
     {
       tabName: "Mission And Visson",
@@ -46,16 +60,33 @@ export const Header = () => {
         location.pathname === "/accessibility-reports" ? "underline" : "none",
     },
   ];
+  const profileMenue = [
+    {
+      title: "Logout",
+      onClick: () => routeTo(" "),
+    },
+    {
+      title: "Profile",
+      onClick: () => routeTo(" "),
+    },
+  ];
 
   return (
     <div className="Header-Parrent">
       <div className="first-child">
-        <div className="child-1  d-flex justify-content-between align-items-center p-2 px-5 "
-        style={{backgroundColor:location.pathname==="/" ? "white" : webColor.themeColor   }}>
+        <div
+          className="child-1  d-flex justify-content-between align-items-center p-2 px-5 "
+          style={{
+            backgroundColor:
+              location.pathname === "/" ? "white" : webColor.themeColor,
+          }}
+        >
           <div className=" d-flex justify-content-between    socialMediaDivStyle">
-            <img src={   location.pathname === "/"
-                          ? haderLogo
-                          : bannerLogowhite } width="150px" alt="logo" />
+            <img
+              src={location.pathname === "/" ? haderLogo : bannerLogowhite}
+              width="150px"
+              alt="logo"
+            />
           </div>
 
           <div className="child-2 d-flex  ">
@@ -74,7 +105,7 @@ export const Header = () => {
                     to={items.link}
                   >
                     <div
-                    className="hoverEffectControl"
+                      className="hoverEffectControl"
                       // style={{borderBottom:"1px solid green"}}
                       style={{ textDecoration: items.style }}
                     >
@@ -86,22 +117,60 @@ export const Header = () => {
               );
             })}
           </div>
-          <div className="child-3 ">
-            <img src={   location.pathname === "/"
-                          ? profileImage
-                          : profilewhiteImage } width="25px" className="mx-3" alt="logo" />
-            <img
-             src={   location.pathname === "/"
-                ? menueImage
-                : menueWhite } 
-              className="social-media-icons"
-              width="22px"
-              alt="logo"
-            />
+          <div className="child-3 d-flex ">
+            <div
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={(e) => handleClick(e, "profile")}
+            >
+              <img
+                src={
+                  location.pathname === "/" ? profileImage : profilewhiteImage
+                }
+                width="25px"
+                className="mx-3"
+                alt="logo"
+              />
+            </div>
+            <div
+              ////popup//
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+
+              /////close
+            >
+              <img
+                src={location.pathname === "/" ? menueImage : menueWhite}
+                className="social-media-icons"
+                width="22px"
+                alt="logo"
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className="d-flex w-100 justify-content-center bg-warning second-Child "></div>
+      <div className="d-flex w-100 justify-content-center bg-warning second-Child ">
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          {(isPopupName === "profile" ? profileMenue : profileMenue)?.map(
+            (item) => {
+              return <MenuItem onClick={handleClose}>{item.title}</MenuItem>;
+            }
+          )}
+        </Menu>
+      </div>
     </div>
   );
 };

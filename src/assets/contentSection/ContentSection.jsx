316225e1861a756de77b971232385398f7cc4596
unknown <saimeshahzad7@gmail.com>
2024-09-26 05:26:10 -0700

    styling added like animartion

--- src/assets/contentSection/ContentSection.jsx
+++ src/assets/contentSection/ContentSection.jsx
@@ -11,31 +11,33 @@ export const ContentSection = ({
         {data?.map((item) => {
           return (
             <div
-            //   className="d-flex "
+              //   className="d-flex "
               className={item.className}
             >
               {/* <div className="text-section  text-center  "> */}
-              <div className= "text-section d-flex align-items-center col-lg-6 col-md-6 col-sm-12 " >
-                
+              <div className="text-section  d-flex align-items-center col-lg-6 col-md-6 col-sm-12 "
+                data-aos="fade-up"
+               >
+
                 <div className=" w-100">
-                <h1>{item.heading}</h1>
-                <h5>{item.subHeading}</h5>
-                <div className="">{item.text}</div>
+                  <h1>{item.heading}</h1>
+                  <h5>{item.subHeading}</h5>
+                  <div className="">{item.text}</div>
                 </div>
               </div>
-              
-              
+
+
               {/* <div className="  img-section "> */}
-              <div className= {item.possition === "left" ?  "img-section col-lg-6 col-md-6 col-sm-12 d-flex align-items-center" :"col-lg-6 col-md-6 col-sm-12 d-flex justify-content-end align-items-center"}>
-                {item.img ? 
-                <img src={item.img} width="80%" height="400px" alt="home-1" /> : ""}
+              <div data-aos="fade-right" className={item.possition === "left" ? "img-section col-lg-6 col-md-6 col-sm-12 d-flex align-items-center" : "col-lg-6 col-md-6 col-sm-12 d-flex justify-content-end align-items-center"}>
+                {item.img ?
+                  <img src={item.img} width="80%" height="400px" alt="home-1" /> : ""}
               </div>
               {/* ///extra content section */}
 
-                        {item.extraContent && <div>
-                            {item.extraContent}
-                            
-                            </div>}
+              {item.extraContent && <div>
+                {item.extraContent}
+
+              </div>}
               {/* ///closed/// */}
             </div>
           );

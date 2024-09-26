4438a49ad4175eb0b482a82bf336cb165edd861b
unknown <saimeshahzad7@gmail.com>
2024-09-26 05:24:35 -0700

    aos librray added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Index } from './pages/main/Index';
+import { useLocation } from 'react-router-dom';
+
+
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+      window.AOS.init({
+          duration: 1000,
+          offset: 200,
+      });
+  }, [location]);
+
+   
   return (
     <div className="">
       {/* <AppRoutes /> */}
